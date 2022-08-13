7fa8b1de21daa865ccef260a5fe9abeedd5d61b0
Jonatan Rask <jonatan16.veloz@gmail.com>
2022-08-13 12:25:54 -0400

    Programa atualizado

--- Trabalhando-com-arquivos/valor/src/application/Program.java
+++ Trabalhando-com-arquivos/valor/src/application/Program.java
@@ -1,15 +1,28 @@
 package application;
-
+/*
+Fazer um programa para ler o caminho de um arquivo .csv
+contendo os dados de itens vendidos. Cada item possui um
+nome, preço unitário e quantidade, separados por vírgula. Você
+deve gerar um novo arquivo chamado "summary.csv", localizado
+em uma subpasta chamada "out" a partir da pasta original do
+arquivo de origem, contendo apenas o nome e o valor total para
+aquele item (preço unitário multiplicado pela quantidade),
+conforme exemplo.
+ */
 import model.service.WriterService;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        String pathReader = "F:\\Estudos\\DSLearn\\Java-POO\\Trabalhando-com-arquivos\\valor\\sold\\items-sold.csv";
-        String pathWriter = "F:\\Estudos\\DSLearn\\Java-POO\\Trabalhando-com-arquivos\\valor\\sold\\out\\summary.csv";
+        System.out.print("Enter directory path: ");
+        String directory = sc.nextLine();
+        String pathReader = directory + "items-sold.csv";
+        String pathWriter =  directory + "out\\summary.csv";
 
         WriterService writerService = new WriterService();
         writerService.writerLine(pathWriter, pathReader);
